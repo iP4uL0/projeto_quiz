8660ef44c88200d2e3910aefd310d3e7817280df
iP4uL0 <paulobapsantos@gmail.com>
2025-04-29 11:47:35 -0300

    mudanças das cores

--- back_end/bd.js
+++ back_end/bd.js
@@ -1,5 +1,5 @@
 import postgres from "postgres";
 
-const sql = postgres('postgres://root:root@192.168.1.15:5432/Nathalia')
+const sql = postgres('postgres://root:root@192.168.1.15:5432/pauloA')
 
 export default sql;
